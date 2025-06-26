687ec460ee9bbff687bbe26078c8bb6277a92c64
Abdelrahman-Samirr <abdelrahmansamir641@gmail.com>
2025-06-27 01:37:16 +0300

    update hoisting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,20 @@
 import { Routes } from '@angular/router';
-import { Nav } from './nav/nav';
 import { Products } from './products/products';
 import { LoginPage } from './login-page/login-page';
 import { RegisterPage } from './register-page/register-page';
 import { ProductDetail } from './product-detail/product-detail';
 import { Cart } from './cart/cart';
+import { NotFound } from './not-found/not-found';
 
 
 
 export const routes: Routes = [
+
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: Products },
-  { path: 'a', component: Nav },
   { path: 'login', component: LoginPage },
   { path: 'register', component: RegisterPage },
   { path: 'product/:id', component: ProductDetail },
-  { path: 'cart', component: Cart }
+  { path: 'cart', component: Cart },
+  { path: '**', component: NotFound }
 ];
